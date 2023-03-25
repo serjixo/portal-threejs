6ccd5d7d93cdff8fa80188d3dd598cd08729ad9f
Sergio Jimenez <serjicho@hotmail.com>
2023-03-25 18:45:12 +0100

    cleans vertexShader

--- src/experience/world/shaders/fireflies/firefliesVertex.glsl
+++ src/experience/world/shaders/fireflies/firefliesVertex.glsl
@@ -1,11 +1,5 @@
-//void main()
-//{
-//    gl_Position =  vec4(position, 1.0);
-//}
 void main() {
     vec4 modelPosition = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-//    vec4 viewPosition = modelPosition * viewMatrix;
-//    vec4 projectionPosition = viewPosition * projectionMatrix;
     gl_Position = modelPosition;
     gl_PointSize = 40.0;
 }
